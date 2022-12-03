4c7cb1b5790ad7c9455fcf00848f967b7a90a479
MaheshwarReddyGoli <MaheshwarReddyGoli>
2022-12-03 22:04:07 +0000

    Updated CleaningNotebook

--- CleaningNotebook.py
+++ CleaningNotebook.py
@@ -1,47 +1,31 @@
 # Databricks notebook source
-pip install adlfs
-
-# COMMAND ----------
-
-pip install fsspec
-
-# COMMAND ----------
-
-# importing libraries
-import pandas as pd
-
-# COMMAND ----------
-
 # mounting azure blob storage to azue databricks using SAS Token
 spark.conf.set("fs.azure.account.auth.type.storagebigfiveproject.dfs.core.windows.net", "SAS")
 spark.conf.set("fs.azure.sas.token.provider.type.storagebigfiveproject.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.storagebigfiveproject.dfs.core.windows.net", "sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2022-12-13T13:26:40Z&st=2022-12-03T05:26:40Z&spr=https&sig=NpzxdT1%2BvdxM6iZv6ouzrHA7638zw4gfdki%2BcigIaUE%3D")
+spark.conf.set("fs.azure.sas.fixed.token.storagebigfiveproject.dfs.core.windows.net", "sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2022-12-22T02:46:51Z&st=2022-12-03T18:46:51Z&spr=https&sig=ag5mOx7K2Sg%2FcnpBqySOwjWP4MeZzTy3%2FnS7OEFj93s%3D")
 
 # COMMAND ----------
 
 # reading json file form landing zone
-ParkingBaySensors_df = spark.read.json("abfs://landing@storagebigfiveproject.dfs.core.windows.net/ParkingBaySensors.json")
-CarParkBayRestrictions_df = spark.read.json("abfs://landing@storagebigfiveproject.dfs.core.windows.net/CarParkBayRestrictions.json")
-
-ParkingBaySensors_pdf = ParkingBaySensors_df.toPandas()
-
+parkingbaysensors_df = spark.read.json("abfs://bigfiveproject@storagebigfiveproject.dfs.core.windows.net/landing/ParkingBaySensors.json")
+carparkbayrestrictions_df = spark.read.json("abfs://bigfiveproject@storagebigfiveproject.dfs.core.windows.net/landing/CarParkBayRestrictions.json")
 
 # COMMAND ----------
 
-ParkingBaySensors_pdf.isnull().sum()
+# Cleaning data by dropDuplicates to remove duplicates
+parkingbaysensors_sdf = parkingbaysensors_df.dropDuplicates()
+carparkbayrestrictions_sdf = carparkbayrestrictions_df.dropDuplicates()
 
 # COMMAND ----------
 
-ParkingBaySensors_dup_df = ParkingBaySensors_pdf.drop_duplicates(subset=['bay_id'])
+# Write cleaned data into interim folder
+parkingbaysensors_sdf.coalesce(1).write.json("abfs://bigfiveproject@bigfiveprojectstorage.dfs.core.windows.net/interim/parkingbaysensors")
+# parkingbays_sdf.coalesce(1).write.json("abfs://melbourne@bigfiveprojectstorage.dfs.core.windows.net/interim/parkingbays")
+carparkbayrestrictions_sdf.coalesce(1).write.json("abfs://bigfiveproject@bigfiveprojectstorage.dfs.core.windows.net/interim/carparkbayrestrictions")
+# parkingbaysensors_sdf.coalesce(1).write.format('json').save("abfs://melbourne@bigfiveprojectstorage.dfs.core.windows.net/interim/test")
 
 # COMMAND ----------
 
-ParkingBaySensors_sdf = ParkingBaySensors_dup_df.dropna()
-
-# COMMAND ----------
-
-ParkingBaySensors_sdf.to_json("abfs://interim@storagebigfiveproject.dfs.core.windows.net/ParkingBaySensors")
-
-# COMMAND ----------
-
-
+#parkingbaysensors_sdf.write.format('json').mode("overwrite").option("header","true").save("abfs://melbourne@storagebigfiveproject.dfs.core.windows.net/interim/parkingbaysensors")
+#parkingbays_sdf.write.format('json').mode("overwrite").option("header","true").save("abfs://melbourne@storagebigfiveproject.dfs.core.windows.net/interim/parkingbays")
+#carparkbayrestrictions_sdf.write.format('json').mode("overwrite").option("header","true").save("abfs://melbourne@storagebigfiveproject.dfs.core.windows.net/interim/carparkbayrestrictions")
